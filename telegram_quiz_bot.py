b0a21b08be290486003ff8d1a3381728d00ee991
Taoboshi <muzzey8@gmail.com>
2026-04-28 18:59:46 +0300

    Add files via upload

--- telegram_quiz_bot.py
+++ telegram_quiz_bot.py
@@ -9,7 +9,7 @@ from threading import Thread
 
 from flask import Flask
 
-from telegram import BotCommand, InputFile, Update, InlineKeyboardMarkup
+from telegram import BotCommand, InputFile, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     ApplicationBuilder,
     CallbackQueryHandler,
